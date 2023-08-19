23777dd90dabdbec18dda8193cdc677b7ef00f99
Vagner Oliveira <vagnerjuniordev@hotmail.com>
2023-08-19 14:00:56 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+var setadireita = window.document.getElementById("setadireita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+var setaesquerda = window.document.getElementById("setaesquerda") 
+
+function RolarParaDireita(){
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    setaesquerda.style = "display:flex; margin-top:55px"
+    setadireita.style = "display:none"
+}
+function RolarParaEsquerda(){
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    setadireita.style = "display:flex; margin-top:54px"
+    setaesquerda.style = "display:none"
+
+}
+
+
+
